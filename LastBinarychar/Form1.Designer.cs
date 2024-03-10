862470b7cfa834a179db141b02c15ddfc7db6cf9
John Doe <106913591+ZiadAhmed-11@users.noreply.github.com>
2024-03-10 23:05:06 +0200

    Negative Database Characters datasets

---
+++ LastBinarychar/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace LastBinarychar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.BinaryOutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(126, 105);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // BinaryOutputTextBox
+            // 
+            this.BinaryOutputTextBox.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.BinaryOutputTextBox.Location = new System.Drawing.Point(12, 214);
+            this.BinaryOutputTextBox.Name = "BinaryOutputTextBox";
+            this.BinaryOutputTextBox.Size = new System.Drawing.Size(438, 224);
+            this.BinaryOutputTextBox.TabIndex = 1;
+            this.BinaryOutputTextBox.Text = "";
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(232, 104);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(75, 23);
+            this.ConvertButton.TabIndex = 2;
+            this.ConvertButton.Text = "insert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(546, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(652, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.richTextBox1.Location = new System.Drawing.Point(456, 214);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 224);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter the character :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label2.Location = new System.Drawing.Point(11, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Negative Database :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.BinaryOutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.RichTextBox BinaryOutputTextBox;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LastBinarychar/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LastBinarychar
+{
+    public partial class Form1 : Form
+    {
+        private string[] binaryArray = { "01000001", "01000010", "01000011", "01000100" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            char inputChar = InputTextBox.Text.ToUpper()[0];
+
+            if (inputChar >= 'A' && inputChar <= 'D')
+            {
+                int index = inputChar - 'A';
+
+                binaryArray[index] = null;
+                UpdateArrayDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a character between A to D.");
+            }
+        }
+
+        private void UpdateArrayDisplay()
+        {
+            BinaryOutputTextBox.Clear();
+
+            foreach (string binary in binaryArray)
+            {
+                if (binary != null)
+                {
+                    BinaryOutputTextBox.AppendText(binary + Environment.NewLine);
+                }
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char inputChar = textBox1.Text.ToUpper()[0];
+
+            if (inputChar >= 'A' && inputChar <= 'D')
+            {
+                int index = inputChar - 'A';
+
+                if (binaryArray[index] != null)
+                {
+                    richTextBox1.Text=($"Binary number for '{inputChar}' does not exists in the database.");
+                }
+                else
+                {
+                    richTextBox1.Text=($"Binary number for '{inputChar}' exist in the database.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a character between A to D.");
+            }
+        }
+        
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
